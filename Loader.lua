6aa74268f94b4d24c454f54b5a9a2757043e1741
Masurix <110713912+Masurix@users.noreply.github.com>
2023-02-11 11:23:16 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,6 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Masurix/Uria/main/"..({
-  [994732206] = "BloxFurit",  
+  [994732206] = "BloxFurit",
+  [9285238704] = "Raceclicker",
   [2788229376] = "DaHood",
 })[game.GameId]))()
